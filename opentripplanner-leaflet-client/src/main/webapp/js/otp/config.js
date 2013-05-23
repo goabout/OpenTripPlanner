2628e222e1651dbf219964f647740d96458b5dd6
Jasper Hartong <jasper@calendar42.com>
2013-05-23 22:25:24 +0200

    changed the config to make miles the default again

--- opentripplanner-leaflet-client/src/main/webapp/js/otp/config.js
+++ opentripplanner-leaflet-client/src/main/webapp/js/otp/config.js
@@ -69,7 +69,7 @@ otp.config = {
     showLogo            : true,
     showTitle           : true,
     showModuleSelector  : true,
-    metric              : true,
+    metric              : false,
 
 
     /**
